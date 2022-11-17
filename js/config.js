196c80a31a2fb33704252fea54617c5508587193
Eduardo Mendoza <75950842+Lalitoza@users.noreply.github.com>
2022-11-17 14:51:14 -0600

    Update config.js
    add my personal code

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId: 'us-west-2_Ygj757EKL',
+        userPoolClientId: '6uo55buu3d36pk1fgbrvel5lbf',
+        region: 'us-west-2'
     },
     api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://9jjl1b7j62.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
